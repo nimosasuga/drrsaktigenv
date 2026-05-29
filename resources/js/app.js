496634b633f0e9a17a552f10cc0ebb410ad90123
Nimo Sasuga <86402024+nimosasuga@users.noreply.github.com>
2026-05-30 03:34:36 +0700

    feat: load update job withdrawn asset blocker

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,4 @@ import "./bootstrap";
 import "./update-job-recommendation-history";
 import "./penarikan-menu-link";
 import "./penarikan-form-enhancer";
+import "./update-job-withdrawn-asset-blocker";
