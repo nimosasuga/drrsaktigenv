b21cc07714b5e65e0d2085c52d5b9c6012c26af6
Nimo Sasuga <86402024+nimosasuga@users.noreply.github.com>
2026-05-30 14:20:27 +0700

    fix: load command center import csv trigger

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import "./penarikan-form-enhancer";
 import "./update-job-withdrawn-asset-blocker";
 import "./update-job-field-options";
 import "./update-job-preventive-maintenance-check";
+import "./command-center-import-trigger";
 import "./update-job-copy-report";
 import "./operational-share-report";
 import "./update-job-detail-extra-fields";
