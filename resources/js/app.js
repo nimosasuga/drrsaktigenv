9a62c3d7ff76ff0cbc8d658506eb5f9e9d1e3d07
Nimo Sasuga <86402024+nimosasuga@users.noreply.github.com>
2026-05-30 12:06:31 +0700

    fix: disable unstable update job extra field injector

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,5 +8,4 @@ import "./update-job-withdrawn-asset-blocker";
 import "./update-job-field-options";
 import "./update-job-copy-report";
 import "./operational-share-report";
-import "./update-job-extra-fields";
 import "./update-job-detail-extra-fields";
