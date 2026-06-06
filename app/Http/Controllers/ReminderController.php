af2204f448619b3fa117595d8dcd6661131664af
Nimo Sasuga <86402024+nimosasuga@users.noreply.github.com>
2026-06-06 18:13:46 +0700

    feat: add reminder controller

---
+++ app/Http/Controllers/ReminderController.php
@@ -0,0 +1,22 @@
+<?php
+// PATH FILE: app/Http/Controllers/ReminderController.php
+
+namespace App\Http\Controllers;
+
+use App\Support\ReminderSummaryService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReminderController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+        $filter = $request->query('filter');
+
+        $summary = ReminderSummaryService::summaryForUser($user);
+        $reminders = ReminderSummaryService::listForUser($user, $filter, 50);
+
+        return view('reminders.index', compact('summary', 'reminders', 'filter'));
+    }
+}
